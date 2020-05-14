aa38c3e891ad246e0aaa9dd8e0cb70cd41166aba
Bruno Santos <brunosansp@outlook.com>
2020-05-14 12:27:50 -0300

    Persistindo a entidade

--- src/main/java/br/com/alura/jpa/testes/CriaTabelaConta.java
+++ src/main/java/br/com/alura/jpa/testes/CriaTabelaConta.java
@@ -1,13 +1,11 @@
 package br.com.alura.jpa.testes;
 
-// import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class TesteCriaTabelas {
+public class CriaTabelaConta {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
-        // EntityManager createEntityManager = emf.createEntityManager();
         emf.close();
     }
     
